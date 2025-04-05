c1aeab5a39ce2e4acb5a1e92a32481c1ac0e5235
WoollyBeardBear <peterakluth@gmail.com>
2025-04-05 14:42:55 -0600

    fixed link, added registration page

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello world<p>"
+def index():
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
