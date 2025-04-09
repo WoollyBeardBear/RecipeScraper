10a8c6d137256f577a1ac4756d6c1ed13c437b59
WoollyBeardBear <peterakluth@gmail.com>
2025-04-09 17:15:25 -0600

    developing spider

--- recipescraper/recipescraper/spiders/recipespider.py
+++ recipescraper/recipescraper/spiders/recipespider.py
@@ -8,4 +8,21 @@ class RecipespiderSpider(scrapy.Spider):
 
     def parse(self, response):
         
-        ingredients = respone.css('ul')
+        ingredients = response.css('a.wprm-recipe-ingredient-link::text').getall()
+        amounts = response.css('span.wprm-recipe-ingredient-amount::text').getall()
+        instructions = response.css('span::text').getall()
+        instruct_list = []
+        # list of ingredients with their amounts
+        i_list = {}
+        for i in range(0, len(ingredients)):
+            try:
+                i_list[ingredients[i]] = amounts[i]
+            except:
+                i_list[ingredients[i]] = 0
+        
+        for entry in instructions:
+            if (entry[0] == 1 or entry[0] == 2 or entry[0] == 3 or entry[0] == 4 or entry[0] == 5 or entry[0] == 6 or entry[0] == 7) and len(entry) > 1:
+                instruct_list.append(entry)
+
+        yield [i_list, instruct_list]
+
